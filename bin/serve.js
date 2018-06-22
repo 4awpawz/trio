d396ea8de81a7b312cb3022edfedd03fbcf8a3ec
Jeff Schwartz <jefftschwartz@gmail.com>
2018-06-22 16:28:22 -0400

    integrated browser-sync into project: created bin folder for commands moved cli.js to ./bin create command serve via ./bin/serve.js to run browser-sync

---
+++ bin/serve.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+// Create a Browsersync instance
+var bs = require("browser-sync").create();
+
+// Listen to change events on HTML and reload
+bs.watch("public/**/*.html").on("change", bs.reload);
+
+// Provide a callback to capture ALL events to CSS
+// files - then filter for 'change' and reload all
+// css files on the page.
+bs.watch("public/css/*.css", function (event, file) {
+    if (event === "change") {
+        bs.reload("*.css");
+    }
+});
+
+// Now init the Browsersync server
+bs.init({
+    server: "./public"
+});
