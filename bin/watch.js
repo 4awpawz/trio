b328049da66820135131810869da73b3e0178796
Jeff Schwartz <jefftschwartz@gmail.com>
2018-06-24 12:25:23 -0400

    naive implementation of chokidar

--- bin/watch.js
+++ bin/watch.js
@@ -1,46 +1,65 @@
 #!/usr/bin/env node
 const chokidar = require("chokidar");
+const run = require("../index.js");
 
 // // One-liner for current directory, ignores .dotfiles
 // chokidar.watch("source").on("all", (event, path) => {
 //     console.log(event, path);
 // });
 
-var watcher = chokidar.watch("source").on("all", (event, path) => {
-    console.log(event, path);
+// var watcher = chokidar.watch("source").on("all", (event, path) => {
+//     console.log(event, path);
+// });
+
+var watcher = chokidar.watch("source/**", {
+    ignoreInitial: true
 });
 
 // Something to use when events are received.
 var log = console.log.bind(console);
+
 // Add event listeners.
 watcher
-    .on("add", path => log(`File ${path} has been added`))
-    .on("change", path => log(`File ${path} has been changed`))
-    .on("unlink", path => log(`File ${path} has been removed`));
+    .on("add", async path => {
+        log("******************* here *****************");
+        log(`File ${path} has been added`);
+        await run().catch();
+    })
+    .on("change", async path => {
+        log("******************* here *****************");
+        log(`File ${path} has been changed`);
+        await run().catch();
+    })
+    .on("unlink", async path => {
+        log("******************* here *****************");
+        log(`File ${path} has been removed`);
+        await run().catch();
+    });
 
 // More possible events.
-watcher
-    .on("addDir", path => log(`Directory ${path} has been added`))
-    .on("unlinkDir", path => log(`Directory ${path} has been removed`))
-    .on("error", error => log(`Watcher error: ${error}`))
-    .on("ready", () => log("Initial scan complete. Ready for changes"))
-    .on("raw", (event, path, details) => {
-        log("Raw event info:", event, path, details);
-    });
+// watcher
+//     .on("addDir", path => log(`Directory ${path} has been added`))
+//     .on("unlinkDir", path => log(`Directory ${path} has been removed`))
+//     .on("error", error => log(`Watcher error: ${error}`))
+//     .on("ready", () => log("Initial scan complete. Ready for changes"))
+//     .on("raw", (event, path, details) => {
+//         log("Raw event info:", event, path, details);
+//     });
 
 // "add", "addDir" and "change" events also receive stat() results as second
 // argument when available: http://nodejs.org/api/fs.html#fs_class_fs_stats
-watcher.on("change", (path, stats) => {
-    if (stats) console.log(`File ${path} changed size to ${stats.size}`);
-});
+// watcher.on("change", (path, stats) => {
+//     if (stats) console.log(`File ${path} changed size to ${stats.size}`);
+// });
 
 // // Watch new files.
 // watcher.add("new-file");
 // watcher.add(["new-file-2", "new-file-3", "**/other-file*"]);
 
 // Get list of actual paths being watched on the filesystem
-var watchedPaths = watcher.getWatched();
-console.debug("Watching", watchedPaths);
+setTimeout(() => {
+    log("Watching", watcher.getWatched());
+}, 1000);
 
 // // Un-watch some files.
 // watcher.unwatch("new-file*");
@@ -49,7 +68,7 @@ console.debug("Watching", watchedPaths);
 // watcher.close();
 
 // // Full list of options. See below for descriptions. (do not use this example)
-// chokidar.watch("file", {
+//chokidar.watch("file", {
 //     persistent: true,
 
 //     ignored: "*.txt",
