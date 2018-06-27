0a4b93137f52ab143ee6e13193b8202caabecfd9
Jeff Schwartz <jefftschwartz@gmail.com>
2018-06-26 21:31:12 -0400

    o now catching unhandled promise rejections o temporarily linking buster to the project o set verbose configuration option to false o using setInterval timer to call watcher.getWatched()

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// require("make-promises-safe"); // installs an 'unhandledRejection' handler
 const generate = require("./lib/pipeline");
 
-module.exports = async () => generate();
+module.exports = async () => generate().catch(() => { });
