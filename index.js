fb31de15b9bf93a49cf5ac81e6c295bdb53420f7
Jeff Schwartz <jefftschwartz@gmail.com>
2018-07-02 12:28:48 -0400

    top level catch now logs error to console

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const generate = require("./lib/pipeline");
 
 // main entry point with "top level" promise rejection handler
 // to prevent "unhandled promise rejection" errors
-module.exports = async () => generate().catch(() => { });
+module.exports = async () => generate().catch((e) => { console.log(e); });
