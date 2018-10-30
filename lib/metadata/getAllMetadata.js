4awpawz/trio

commit 74f64aed15288176234a49709f2a5ee31264ba0c

now discarding fragment from build process when front matter is missing title or template properties #41

lib/metadata/getAllMetadata.js
@@ -12,6 +12,8 @@ const {
     toArray
 } = require("../utils");
 
+const log = console.log.bind(console);
+
 const blogMetadata = metadata => {
     if (metadata.path.startsWith(config.sourceTag)) {
         metadata.matter.data.forTag = metadata.matter.data.forTag
@@ -32,25 +34,10 @@ const blogMetadata = metadata => {
     return metadata;
 };
 
-const commonMetadata = path => {
-    const metadata = {};
-    metadata.path = path;
-    metadata.matter = matter(read(metadata.path), {
-        delimiters: config.fmDelimiters,
-        excerpt: true,
-        excerpt_separator: config.fmExcerptSeparator
-    });
-    if (typeof metadata.matter.data.template === "undefined") {
-        console.log(`Page fragment missing "template" property: ${path}`);
-        process.abort();
-    }
-    if (typeof metadata.matter.data.title === "undefined") {
-        console.log(`Page fragment missing "title" property: ${path}`);
-        process.abort();
-    }
+const commonMetadata = metadata => {
     metadata.matter.data.template = `${config.templates}${sep}${metadata.matter.data.template}`;
     metadata.matter.data.appendToTarget = metadata.matter.data.appendToTarget || false;
-    metadata.matter.content = parse(path).ext === "html"
+    metadata.matter.content = parse(metadata.path).ext === "html"
         ? metadata.matter.content
         : marked(metadata.matter.content);
     metadata.id = getUniqueId();
@@ -59,13 +46,39 @@ const commonMetadata = path => {
     return metadata;
 };
 
+const isMatterMissingRequiredProperties = (matter, path) => {
+    const errMsg = "Discarding page fragment - missing";
+    let missing = false;
+    // check for missing title and template properties
+    if (typeof matter.data.template === "undefined" || !matter.data.template) {
+        log(`${errMsg} "template" property: ${path}`);
+        missing = true;
+    }
+    if (typeof matter.data.title === "undefined" || !matter.data.title) {
+        log(`${errMsg} "title" property: ${path}`);
+        missing = true;
+    }
+    return missing;
+};
+
 module.exports = () => {
-    return glob.sync("source/fragments/**/{*.html,*.md}", {
+    const metadatas = [];
+    glob.sync("source/fragments/**/{*.html,*.md}", {
         ignore: process.env.TRIO_ENV === "release" &&
             "source/fragments/blog/articles/**/_*.*" ||
             ""
-    }).map(path => {
-        const metadata = commonMetadata(path);
-        return blogMetadata(metadata);
+    }).forEach(path => {
+        const m = matter(read(path), {
+            delimiters: config.fmDelimiters,
+            excerpt: true,
+            excerpt_separator: config.fmExcerptSeparator
+        });
+        if (!isMatterMissingRequiredProperties(m, path)) {
+            // fragments has both title & template prorties
+            metadatas.push({ path, matter: m });
+        }
+    });
+    return metadatas.map(metadata => {
+        return blogMetadata(commonMetadata(metadata));
     });
 };

commit 69b34bbdf2ae762fe9e5574552887da3fe315840

changed "html > ".html" fixes error introduced in 74f64aed

lib/metadata/getAllMetadata.js
@@ -37,7 +37,7 @@ const blogMetadata = metadata => {
 const commonMetadata = metadata => {
     metadata.matter.data.template = `${config.templates}${sep}${metadata.matter.data.template}`;
     metadata.matter.data.appendToTarget = metadata.matter.data.appendToTarget || false;
-    metadata.matter.content = parse(metadata.path).ext === "html"
+    metadata.matter.content = parse(metadata.path).ext === ".html"
         ? metadata.matter.content
         : marked(metadata.matter.content);
     metadata.id = getUniqueId();