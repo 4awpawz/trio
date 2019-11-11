c1f525718eadf296d3541036c58c6d24112106e1
Jeff Schwartz <jefftschwartz@gmail.com>
2019-11-11 07:24:50 -0500

    for each build, now surrounding all output to the terminal between 2 lines of asterisks

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const generate = require("./lib/generator");
 const { log } = require("./lib/utils");
 
 module.exports = async (path) => {
+    const seperator = "*********************************************************";
+    log(seperator);
     if (process.env.TRIO_ENV_buildType === "release") {
         log("building site for release");
     } else {
         log("building site for development");
     }
     await generate(path).catch((e) => { log(e); });
+    log(seperator);
 };
